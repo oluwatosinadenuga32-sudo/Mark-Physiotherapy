8eb545eb51d7d64774323030caae710b4c72b098
Adenuga Joshua Oluwatosin <adenugaa963@gmail.com>
2026-02-13 11:17:01 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById("menu");
+const nav = document.querySelector(".links");
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
+
+
